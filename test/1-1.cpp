31cada3ed6618ca41a510c1deb96621769969e04
hhhhzzzzzz <2724978465@qq.com>
2023-09-08 19:12:30 +0800

    三次

--- test/1-1.cpp
+++ test/1-1.cpp
@@ -7,8 +7,14 @@ int main() {
 	char ch = 'a';
 	char cx = 's';
 	cout << ch << endl;
-	cout << cx << endl;
+	cout << (int)cx << endl;
 	cout << sizeof(ch) << endl;
+	cout << "hello W\t";
+	cout << "hello W\n";
+
+	system("pause");
+
+	return 0;
 
 
 }
---
+++ test/转意字符.cpp
@@ -0,0 +1,12 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//
+//	cout << "hello W\t";
+//	cout << "hello W\n";
+//	system("pause");
+//
+//	return 0;
+//}
